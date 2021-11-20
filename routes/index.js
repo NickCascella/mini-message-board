cb177c58315ef57c4c064a1d2ba8df1aaa7b85ba
Nick <cascella3509@gmail.com>
2021-11-19 23:26:16 -0500

    need to style a bit

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    title: "Name",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    title: "Name",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini Message Board", messages: messages });
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    title: req.body.title,
+    user: req.body.user,
+    added: new Date(),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
